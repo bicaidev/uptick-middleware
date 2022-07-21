ca76e4d227bc86575093d300a21b02c4887bccb6
liangke <wow1_panda@163.com>
2022-07-21 13:32:49 +0800

    update main network

--- src/service/loopring/handler.ts
+++ src/service/loopring/handler.ts
@@ -41,8 +41,8 @@ import { bnToBufWithFixedLength } from "./sign/eddsa";
 import axios from "axios";
 // import BigNumber from "bignumber.js";
 // goerli
-const baseRprUrl = "https://uat2.loopring.io/";
-
+const baseRprUrltest = "https://uat2.loopring.io/";
+const baseRprUrl = "https://api3.loopring.io";
 let userExtendApi: UserExtendAPI;
 
 export class LRCHandler {
